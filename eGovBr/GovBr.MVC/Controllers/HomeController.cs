bcec19ca5c9f6af7d6d6d3be05819ee7e8634af1
Luan Queiroz <luanqsouza.17@gmail.com>
2022-05-12 11:21:45 -0300

    First step, butão de login com link

--- eGovBr/GovBr.MVC/Controllers/HomeController.cs
+++ eGovBr/GovBr.MVC/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Cryptography;
+using System.Configuration;
+using RestSharp;
 
 namespace GovBr.MVC.Controllers
 {
@@ -10,6 +13,23 @@ namespace GovBr.MVC.Controllers
     {
         public ActionResult Index()
         {
+            //Random random = new Random();
+            //string state = random.Next().ToString("X");
+            //string nonce = random.Next().ToString("X");
+            //string scope = "openid+(email/phone)+profile+govbr_empresa+govbr_confiabilidades";
+
+            //var client = new RestClient(ConfigurationManager.AppSettings["UrlBase"].ToString() + "authorize");
+            //var request = new RestRequest();
+            //request.AddQueryParameter("response_type", "code");
+            //request.AddQueryParameter("client_id", _clientId);
+            //request.AddQueryParameter("scope", scope);
+            //request.AddQueryParameter("redirect_uri", "http://localhost:12224/Autenticate");
+            //request.AddQueryParameter("nonce", nonce);
+            //request.AddQueryParameter("state", state);
+            //var response = client.ExecuteGetAsync(request).Result;
+
+
+
             return View();
         }
 
---
+++ eGovBr/GovBr.MVC/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace GovBr.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        private string _clientId = ConfigurationManager.AppSettings["ClientId"];
+        private string _clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+
+        public ActionResult Index()
+        {
+            Random random = new Random();
+            string state = random.Next().ToString("X");
+            string nonce = random.Next().ToString("X");
+            string scope = "openid+(email/phone)+profile+govbr_empresa+govbr_confiabilidades";
+            string urlAuthorize = ConfigurationManager.AppSettings["UrlAuthorize"].ToString();
+            string redirectUrl = Url.Encode("http://ana-d.ikon.com.br/proton"); //url de redirecinamento após autenticação
+            string link = $"{urlAuthorize}authorize?" +
+                $"response_type=code&" +
+                $"client_id={_clientId}&" +
+                $"scope={scope}&" +
+                $"redirect_uri={redirectUrl}&" +
+                $"nonce={nonce}&" +
+                $"state={state}";
+            TempData["LinkGovBr"] = link;
+
+            return View();
+        }
+
+        public ActionResult Autenticate(dynamic json)
+        {
+            return View();
+        }
+    }
+}
