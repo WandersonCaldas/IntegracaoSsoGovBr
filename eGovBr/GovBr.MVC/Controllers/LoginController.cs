d7a6ea64a897675b6939593e15d13329783aad07
Wanderson Caldas <wcaldasti@gmail.com>
2022-05-12 13:40:51 -0300

    .

--- eGovBr/GovBr.MVC/Controllers/LoginController.cs
+++ eGovBr/GovBr.MVC/Controllers/LoginController.cs
@@ -17,9 +17,10 @@ namespace GovBr.MVC.Controllers
             Random random = new Random();
             string state = random.Next().ToString("X");
             string nonce = random.Next().ToString("X");
-            string scope = "openid+(email/phone)+profile+govbr_empresa+govbr_confiabilidades";
+            //string scope = "openid+(email/phone)+profile+govbr_empresa+govbr_confiabilidades";
+            string scope = "openid+(email/phone)+profile+govbr_confiabilidades";
             string urlAuthorize = ConfigurationManager.AppSettings["UrlAuthorize"].ToString();
-            string redirectUrl = Url.Encode("http://ana-d.ikon.com.br/proton"); //url de redirecinamento após autenticação
+            string redirectUrl = Url.Encode("http://ana-d.ikhon.com.br/proton/ProtocoloPublico/Home/Index"); //url de redirecinamento após autenticação
             string link = $"{urlAuthorize}authorize?" +
                 $"response_type=code&" +
                 $"client_id={_clientId}&" +
